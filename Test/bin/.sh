5699571bb59d0d8d35209a5425e179fd8bcc40ab
yossef-ashraf <yossefff2001@gmail.com>
2024-07-03 11:00:21 +0300

    use phpunit use terminal

---
+++ Test/bin/.sh
@@ -0,0 +1,5 @@
+composer require phpunit/phpunit
+Run PHPUnit Tests
+./vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist
+./vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist Pattern/Test/AbstractFactoryTest
+./vendor/bin/phpunit Test\AbstractFactoryTest.php
