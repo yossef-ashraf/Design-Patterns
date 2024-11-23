edc11b674c16fc2c2f22773b7f5663153138de88
yossef ashraf <yossefff2001@gmail.com>
2024-11-24 01:50:15 +0200

    chore: Add initial Composer setup with .gitignore and index.php

---
+++ _SOLID/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+// $SingleResponsibilityUser= new APP\SingleResponsibility\SingleResponsibilityUser();
+
+
+// $players = ["Defence","Defence","Defence","Defence","Attack","Attack","Attack"]; // can add more players useing interface
+// $OpenClose= new APP\OpenClose\Matchs($players);
+
+
+// $Liskov= new APP\Liskov\Liskov();
+
+// $exRead= new APP\InterfaceSegregation\Read();
+// $exAllOperations= new APP\InterfaceSegregation\Operations();
+
+
+// $players = ["Defence","Defence","Defence","Defence","Attack","Attack","Attack"]; // can add more players useing interface
+// $DefenceInterfaces= new APP\DependencyInversion\Matchs($players);
+
+
+
